ecfea2cbd9531a83d70f8716bc45ff5bccb21882
Stéphane Bressani <stephane@bressani.dev>
2025-05-06 11:06:04 +0200

    Initial Commit

---
+++ Time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time
+//
+//  Created by Stéphane Bressani on 06.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time/TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeApp.swift
+//  Time
+//
+//  Created by Stéphane Bressani on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
